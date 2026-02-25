1377fc55ad13399690246de8bc247bde8dce4940
wayneme75 <50749920+wayneme75@users.noreply.github.com>
2026-02-25 14:48:45 -0800

    Update azl.ps1

--- azl.ps1
+++ azl.ps1
@@ -8,13 +8,28 @@ do {
 
 #Define the region to use to register your server as Arc device
 #Do not use spaces or capital letters when defining region
-$Region = "usgovvirginia"
+do {
+    $EnvironmentSelection = Read-Host "Select Azure environment (1 = Azure Commercial, 2 = Azure Government)"
+} while ($EnvironmentSelection -notin @('1', '2'))
+
+switch ($EnvironmentSelection) {
+    '1' {
+        $AzEnvironment = "AzureCloud"
+        $ArcCloud = "AzureCloud"
+        $Region = "eastus"
+    }
+    '2' {
+        $AzEnvironment = "AzureUSGovernment"
+        $ArcCloud = "AzureUSGovernment"
+        $Region = "usgovvirginia"
+    }
+}
 
 #Define the tenant you will use to register your machine as Arc device
 $Tenant = (get-azcontext).tenant.id
 
 #Connect to your Azure account and Subscription
-Connect-AzAccount -SubscriptionId $Subscription -TenantId $Tenant -DeviceCode -environment "AzureUSGovernment"
+Connect-AzAccount -SubscriptionId $Subscription -TenantId $Tenant -DeviceCode -Environment $AzEnvironment
 
 #Get the Access Token for the registration
 $ARMtoken = (Get-AzAccessToken -WarningAction SilentlyContinue).Token
@@ -29,5 +44,5 @@ if ($armTokenType -eq 'SecureString') {
 $id = (Get-AzContext).Account.Id
 
 #Invoke the registration script. Use a supported region.
-Invoke-AzStackHciArcInitialization -SubscriptionID $Subscription -ResourceGroup $RG -TenantID $Tenant -Region $Region -Cloud "AzureUSGovernment" -ArmAccessToken $ARMtoken -AccountID $id
+Invoke-AzStackHciArcInitialization -SubscriptionID $Subscription -ResourceGroup $RG -TenantID $Tenant -Region $Region -Cloud $ArcCloud -ArmAccessToken $ARMtoken -AccountID $id
  
